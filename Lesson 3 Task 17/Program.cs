c825f15b70525a70e5dba7f9a80ada80b012ff1f
George <georgij_trakhten@mail.ru>
2022-01-11 17:42:59 +0300

    Lesson 3 Task 17 added

---
+++ Lesson 3 Task 17/Program.cs
@@ -0,0 +1,18 @@
+﻿int ReadIntFromConsole(string msg)
+
+{
+    Console.WriteLine(msg);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int number1 = ReadIntFromConsole("Введите первое число: ");
+int number2 = ReadIntFromConsole("Введите второе число: ");
+
+if((number1 == number2 * number2) || (number2 == number1 * number1))
+{
+    Console.WriteLine("One number is the square amount of the other");
+}
+else
+{
+    Console.WriteLine("One number is not the square amount of the other");
+}
