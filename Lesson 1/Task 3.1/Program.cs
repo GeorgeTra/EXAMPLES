61fb342033610f5dda81e20aa4eb6e994f61ac4f
George <georgij_trakhten@mail.ru>
2022-01-13 16:33:18 +0300

    Lesson 1 folder changed

---
+++ Lesson 1/Task 3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+if (chislo == 1)
+    Console.WriteLine("Понедельник");
+else if (chislo == 2)
+        Console.WriteLine("Вторник");
+    else if (chislo == 3)
+            Console.WriteLine("Среда");
+        else if (chislo == 4)
+                Console.WriteLine("Четверг");
+            else if (chislo == 5)
+                    Console.WriteLine("Пятница");
+                else if (chislo == 6)
+                        Console.WriteLine("Суббота");
+                    else if (chislo == 7)
+                            Console.WriteLine("Воскресенье");
+
