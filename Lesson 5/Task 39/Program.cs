3f46b8d9a8ef668cbf57b48deac88540e83ad336
George <georgij_trakhten@mail.ru>
2022-01-19 13:29:34 +0300

    Lesson 5 added

---
+++ Lesson 5/Task 39/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д.
+
+void FillArray (int[] array, int min, int max)
+{
+    int len = array.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+}       
+
+void PrintArray (int[] array)
+{
+    int len = array.Length;
+
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i],8}");
+    }
+    Console.WriteLine();
+}  
+
+int[] MultElement(int[] array)
+{
+    int len = array.Length;
+    int lenResult = len % 2 == 0 ? len / 2 : len / 2 + 1;
+    int[] temp = new int[lenResult];
+    for (int i = 0; i < lenResult; i++)
+    {
+        temp[i] = array[i] * array[len - 1 - i];
+    }
+    return temp;
+}  
+
+
+int[] array = new int[5];
+FillArray(array, 1, 10);
+PrintArray(array);
+int[] arrResult = MultElement(array);
+PrintArray(arrResult);
+
---
+++ Lesson 5/Task 40/Program.cs
@@ -0,0 +1,30 @@
+﻿// 40. В  Указанном массиве вещественных чисел найдите разницу 
+// между максимальным и минимальным элементом
+
+void FillAndPrintArray(int[] array, int Min, int Max)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(Min, Max + 1);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+(int, int) MinMaxArray(int[] array)
+{
+    int minNumber = array[0];
+    int maxNumber = array[0];
+    foreach (int number in array)
+    {
+        if (number < minNumber) minNumber = number;
+        if (number > maxNumber) maxNumber = number;
+    }
+    return (minNumber, maxNumber);
+}
+
+int[] Array = new int[12];
+FillAndPrintArray(Array, 0, 99);
+(int min, int max) = MinMaxArray(Array);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {max-min}");
+
