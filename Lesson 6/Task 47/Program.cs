08b550ee15efb965c8b4ba37028f4ac39c9b030a
GeorgeTra <georgij_trakhten@mail.ru>
2022-01-23 15:28:58 +0300

    Task 48 added

--- Lesson 6/Task 47/Program.cs
+++ Lesson 6/Task 47/Program.cs
@@ -13,11 +13,24 @@ int[] CreateArray(int number, int min, int max)
     int[] array = new int[number];
     Random rnd = new Random();
     for (int i = 0; i < number; i++)
-        {
-            array[i] = rnd.Next(min, max);
-        }
-        return array;
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
 }
 
+int[] CopyArray (int[] array)
+{
+    int[] resultArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        resultArray[i] = array[i];
+    }
+    return resultArray;
+}
+
+Console.Clear();
 int[] array1 = CreateArray(10, 1, 100);
-PrintArray
+PrintArrayInt(array1);
+Console.WriteLine();
+PrintArrayInt(CopyArray(array1));
---
+++ Lesson 6/Task 48/Program.cs
@@ -0,0 +1,23 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами.
+
+void PrintArrayInt(int[] arrayStr)
+{
+    foreach (var item in arrayStr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[,] CreateArray(int qntRows, int qntColumns, int min, int max)
+{
+    int[,] array = new int[qntRows, qntColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < qntRows; i++)
+    {    for (int j = 0; i < qntColumns; i++)
+        {
+        array[i,j] = rnd.Next(min, max);
+        }
+    }
+    return array;
+}
+
